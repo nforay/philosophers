cee6b07e66352681f7b17757991489ab98f8438c
nforay <nforay@student.42.fr>
2020-11-09 19:25:59 +0100

    Fix headers

--- philo_one/includes/philosopher.h
+++ philo_one/includes/philosopher.h
@@ -6,12 +6,19 @@
 /*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/24 18:46:28 by nforay            #+#    #+#             */
-/*   Updated: 2020/11/09 18:46:20 by nforay           ###   ########.fr       */
+/*   Updated: 2020/11/09 19:06:32 by nforay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHER_H
 # define PHILOSOPHER_H
+
+# define ERROR_1 "Error: arguments expected\n"
+# define ERROR_2 "Error: invalid number_of_philosopher\n"
+# define ERROR_3 "Error: invalid time_to_die\n"
+# define ERROR_4 "Error: invalid time_to_eat\n"
+# define ERROR_5 "Error: invalid time_to_sleep\n"
+# define ERROR_6 "Error: invalid number_of_time_each_philosophers_must_eat\n"
 
 enum	e_state
 {
@@ -30,30 +37,5 @@ typedef struct				s_params
 	size_t					nbr_of_meals;
 	size_t					time;
 }							t_params;
-
-typedef struct				s_philosopher
-{
-	size_t					nbr;
-	size_t					alive;
-	size_t					meals_eaten;
-	pthread_t				thread;
-	enum e_state			state;
-	t_params				*params;
-	pthread_mutex_t			*l_fork;
-	pthread_mutex_t			*r_fork;
-	size_t					starvation;
-	struct s_philosopher	*next;
-}							t_philosopher;
-
-typedef int					(*t_function)(t_philosopher *);
-void						*state_machine(void *arg);
-int							parse_args(int argc, char **argv, t_params *params);
-int							destroy_table(t_philosopher *phi);
-int							destroy_philisophers(t_philosopher *phi,
-								t_philosopher *tmp);
-int							print_error(char *str, int len);
-void						print_status(t_philosopher *phi, size_t nbr,
-								char *str, size_t size);
-size_t						gettime_us(void);
 
 #endif
